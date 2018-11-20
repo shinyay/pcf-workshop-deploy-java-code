b7428bf9b8ddd7961304b5db0aded8eb6fa36cda
shinyay <shinya.com@gmail.com>
2018-11-21 07:51:22 +0900

    Add reweal modules

---
+++ src/main/java/com/example/hellopcf/HelloPcfApplication.java
@@ -0,0 +1,12 @@
+package com.example.hellopcf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPcfApplication.class, args);
+	}
+}
